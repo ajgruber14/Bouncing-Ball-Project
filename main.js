3ca47d0fd221c25593328a5a6c86e484b99485d8
ajgruber14 <104179603+ajgruber14@users.noreply.github.com>
2022-08-15 21:22:03 -0500

    Light & Dark mode!

--- main.js
+++ main.js
@@ -62,24 +62,60 @@ function loop() {
 
 loop();
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+
+
+const menuToggle = document.getElementsByClassName('menu-toggle')[0];
+
+menuToggle.addEventListener('click', () => {
+    document.getElementsByClassName('navbar-links')[0].classList.toggle('active');
 })
 
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle');
-toggle.onclick = function(){
-    toggle.classList.toggle('active')
-    body.classList.toggle('active');
+
+
+
+const modeToggle = document.getElementById('mode-toggle');    
+
+modeToggle.onclick = function(){
+    modeToggle.classList.toggle('dark');
+
+    document.querySelector('body').classList.toggle('dark');
+    document.getElementsByClassName('navbar')[0].classList.toggle('dark');
+        document.getElementsByClassName('logo')[0].classList.toggle('dark');
+        const navLinks = document.getElementsByClassName('nav-link');
+        for (var i=0; i<navLinks.length; i++) {
+            navLinks[i].classList.toggle('dark');
+        }
+        const menuBar = document.getElementsByClassName('menu-bar');
+        for (var i=0; i<menuBar.length; i++) {
+            menuBar[i].classList.toggle('dark');
+        }
+        //need to figure out dark mode for link background-color on hover
+    document.getElementsByClassName('title')[0].classList.toggle('dark');
+    const credit = document.getElementsByClassName('credit');
+        for (var i=0; i<credit.length; i++) {
+            credit[i].classList.toggle('dark');
+        }
+    document.getElementsByClassName('introduction')[0].classList.toggle('dark');
+    document.getElementsByClassName('note')[0].classList.toggle('dark');
+    document.getElementsByClassName('html-container')[0].classList.toggle('dark');
+    document.getElementsByClassName('css-container')[0].classList.toggle('dark');
+    document.getElementsByClassName('js-container')[0].classList.toggle('dark');
+    const snippet = document.getElementsByClassName('snippet');
+        for (var i=0; i<snippet.length; i++) {
+            snippet[i].classList.toggle('dark');
+        }
+    const code = document.getElementsByClassName('code');
+    for (var i=0; i<code.length; i++) {
+        code[i].classList.toggle('dark');
+    }
 }
 
 
 
+
 let scrollpos = window.scrollY;
-let header = document.querySelector('header');;
+let header = document.querySelector('header');
 
 function removeOpacity() {
     header.style.opacity = "0";
